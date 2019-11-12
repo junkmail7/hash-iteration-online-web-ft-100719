6b83b69f43b4544286e3236cc43cb8b8e510a6e6
William Pannell <lpann5@gmail.com>
2019-11-12 15:21:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,17 @@
 # }
 
 def happy_birthday(birthday_kids)
+<<<<<<< HEAD
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
+=======
+  def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+>>>>>>> 11bf8ec2c315eb492456b54da9dffbed5b4120ca
 end
 
 def age_appropriate_birthday(birthday_kids)
